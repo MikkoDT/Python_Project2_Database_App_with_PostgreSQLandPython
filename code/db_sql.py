6f75de5cba8b4ff0cc3fec4541500112516e3917
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-08-13 20:23:57 +0800

    Update db_sql.py

--- code/db_sql.py
+++ code/db_sql.py
@@ -1,4 +1,9 @@
 import psycopg2
 
-con=psycopg2.connect(dbname="studentdb",user="postgres",password="00RaiserG#",host="localhost",port="5432")
-print(con)
+conn=psycopg2.connect(dbname="studentdb",user="postgres",password="00RaiserG#",host="localhost",port="5432")
+cur=conn.cursor()
+#print(conn)
+cur.execute("create table students(student_id serial primary key, name text, address text,age int, number text);")
+print("Student table created")
+conn.commit()
+conn.close()
