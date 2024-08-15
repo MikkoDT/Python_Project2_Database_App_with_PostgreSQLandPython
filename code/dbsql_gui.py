6199eba6d8f9e3dac83245555c2e021c6f4f5ec8
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-08-15 21:53:01 +0800

    Update dbsql_gui.py

--- code/dbsql_gui.py
+++ code/dbsql_gui.py
@@ -1,5 +1,36 @@
 from tkinter import *
 from tkinter import ttk
+import psycopg2
+
+def run_query(query,parameters=()):
+    conn=psycopg2.connect(dbname="studentdb",user="postgres",password="00RaiserG#",host="localhost",port="5432")
+    cur=conn.cursor()
+    query_result = None
+    try:
+        cur.execute(query,parameters)
+        if query.lower().startswith("select"):
+            query_result = cur.fetchall()
+        conn.commit()
+    except psycopg2.Error as e:
+        messagebox.showerror('Database Error',str(e))
+    finally:
+        cur.close()
+        conn.close()
+    return query_result
+
+def refresh_treeview():
+    for item in tree.get_children():
+        tree.delete(item)
+    records = run_query("select * from students;")
+    for record in records:
+        tree.insert('',END,values=record)
+
+def insert_data():
+    query = "insert into students(name,address,age,number) values (%s,%s,%s,%s)"
+    parameters = (name_entry.get(),address_entry.get(),age_entry.get(),phone_entry.get())
+    run_query(query,parameters)
+    messagebox.showinfo("Information","Data inseted successfully!")
+    refresh_treeview()
 
 root = Tk()
 root.title("Student Management System")
@@ -27,7 +58,7 @@ button_frame = Frame(root)
 button_frame.grid(row=1,column=0,pady=5,sticky="ew")
 
 Button(button_frame,text="Create Table").grid(row=0,column=0,padx=5)
-Button(button_frame,text="Add Data").grid(row=0,column=1,padx=5)
+Button(button_frame,text="Add Data",command=insert_data).grid(row=0,column=1,padx=5)
 Button(button_frame,text="Update Data").grid(row=0,column=2,padx=5)
 Button(button_frame,text="Delete Data").grid(row=0,column=3,padx=5)
 
@@ -54,5 +85,8 @@ tree.heading("name",text="Name",anchor=CENTER)
 tree.heading("address",text="Address",anchor=CENTER)
 tree.heading("age",text="Age",anchor=CENTER)
 tree.heading("number",text="Phone Number",anchor=CENTER)
+
+refresh_treeview()
+
 
 root.mainloop()
