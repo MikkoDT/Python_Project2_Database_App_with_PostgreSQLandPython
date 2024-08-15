09fea1dcbdbc94331856cf0228da0599330cd531
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-08-15 20:41:22 +0800

    Update dbsql_gui.py

--- code/dbsql_gui.py
+++ code/dbsql_gui.py
@@ -10,4 +10,16 @@ Label(frame,text="Name: ").grid(row=0,column=0,padx=2,sticky="w")
 name_entry = Entry(frame)
 name_entry.grid(row=0,column=1,pady=2,sticky="ew")
 
+Label(frame,text="Address: ").grid(row=1,column=0,padx=2,sticky="w")
+address_entry = Entry(frame)
+address_entry.grid(row=1,column=1,pady=2,sticky="ew")
+
+Label(frame,text="Age: ").grid(row=2,column=0,padx=2,sticky="w")
+age_entry = Entry(frame)
+age_entry.grid(row=2,column=1,pady=2,sticky="ew")
+
+Label(frame,text="Phone Number: ").grid(row=3,column=0,padx=2,sticky="w")
+phone_entry = Entry(frame)
+phone_entry.grid(row=3,column=1,pady=2,sticky="ew")
+
 root.mainloop()
