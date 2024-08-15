544af71c0cd68a863746f30bb019cf6676849852
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-08-15 20:34:17 +0800

    Update dbsql_gui.py

--- code/dbsql_gui.py
+++ code/dbsql_gui.py
@@ -3,3 +3,7 @@ from tkinter import *
 root = Tk()
 root.title("Student Management System")
 
+frame = LabelFrame(root,text="Student Data")
+frame.grid(row=0,column=0,padx=10,pady=10,sticky="ew")
+
+root.mainloop()
